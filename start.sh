9d75b3e452e62feb394dad6e1059679dacbf48f3
nirjal <nirjalpaudel54312@gmail.com>
2021-07-06 19:35:34 +0545

    fixed vev and no packages error

--- start.sh
+++ start.sh
@@ -1,11 +1,17 @@
-python -m venv virtualenv
-source virtualenv/Scripts/activate
-echo Do you have packages installed [Y/N]?
-read bool
-if [ $bool == [Nn] ]
+if  [ ! -d "virtualenv" ]
 then
-    echo Installing packages
-    echo This may take time
+    python -m venv virtualenv
+fi
+source virtualenv/Scripts/activate
+read -r -p "Do you have packages installed [Y/N]? " bool
+if [ $bool == 'N' ] || [ $bool == 'n' ]
+then
+    echo "Installing packages"
+    echo "This may take time"
+    echo "Be patient"
+    
     pip install -r requirements.txt
 fi
+
+echo "Starting dev server"
 python provathan_backend/manage.py runserver
